774684855d8517929823eb11270b9233d055d1d2
Shahadat Hossain <33016130+sdtHossain@users.noreply.github.com>
2024-03-26 02:04:41 +0600

    changed sidebar text

--- components/AppSidebar.vue
+++ components/AppSidebar.vue
@@ -6,52 +6,52 @@
       <a
         href=""
         class="list-group-item list-group-item-action ps-4 border-0 active-tab"
-        ><i class="ri-home-4-line ms-2 me-3 fs-5"></i>লিপিফিড</a
+        ><i class="ri-home-4-line ms-2 me-3 fs-5"></i>Feed</a
       >
       <a href="" class="list-group-item list-group-item-action ps-4 border-0"
-        ><i class="ri-dashboard-line ms-2 me-3 fs-5"></i>ড্যাশবোর্ড</a
+        ><i class="ri-dashboard-line ms-2 me-3 fs-5"></i>Dashboard</a
       >
       <a href="" class="list-group-item list-group-item-action ps-4 border-0"
-        ><i class="ri-quill-pen-line ms-2 me-3 fs-5"></i>নতুন লিপি</a
+        ><i class="ri-quill-pen-line ms-2 me-3 fs-5"></i>New Post</a
       >
-      <small class="mt-3 ms-4 text-muted">অ্যাক্টিভিটি</small>
+      <small class="mt-3 ms-4 text-muted">Activity</small>
       <a href="" class="list-group-item list-group-item-action ps-4 border-0"
-        ><i class="ri-chat-1-line ms-2 me-3 fs-5"></i>লিপিচ্যাট</a
-      >
-      <a href="" class="list-group-item list-group-item-action ps-4 border-0"
-        ><i class="ri-trophy-line ms-2 me-3 fs-5"></i>লিডারবোর্ড</a
+        ><i class="ri-chat-1-line ms-2 me-3 fs-5"></i>Chat</a
       >
       <a href="" class="list-group-item list-group-item-action ps-4 border-0"
-        ><i class="ri-book-mark-line ms-2 me-3 fs-5"></i>বুকমার্কস</a
+        ><i class="ri-trophy-line ms-2 me-3 fs-5"></i>Leader Board</a
       >
       <a href="" class="list-group-item list-group-item-action ps-4 border-0"
-        ><i class="ri-gift-line ms-2 me-3 fs-5"></i>রিওয়ার্ড</a
-      >
-      <small class="mt-3 ms-4 text-muted">এক্সপ্লোর</small>
-      <a href="" class="list-group-item list-group-item-action ps-4 border-0"
-        ><i class="ri-pencil-ruler-line ms-2 me-3 fs-5"></i>লিপিশিখন</a
+        ><i class="ri-book-mark-line ms-2 me-3 fs-5"></i>Bookmarks</a
       >
       <a href="" class="list-group-item list-group-item-action ps-4 border-0"
-        ><i class="ri-shopping-bag-line ms-2 me-3 fs-5"></i>লিপিশপ</a
+        ><i class="ri-gift-line ms-2 me-3 fs-5"></i>Reward</a
+      >
+      <small class="mt-3 ms-4 text-muted">Explore</small>
+      <a href="" class="list-group-item list-group-item-action ps-4 border-0"
+        ><i class="ri-pencil-ruler-line ms-2 me-3 fs-5"></i>Learning</a
       >
       <a href="" class="list-group-item list-group-item-action ps-4 border-0"
-        ><i class="ri-question-answer-line ms-2 me-3 fs-5"></i>লিপিফোরাম</a
+        ><i class="ri-shopping-bag-line ms-2 me-3 fs-5"></i>Shop</a
       >
       <a href="" class="list-group-item list-group-item-action ps-4 border-0"
-        ><i class="ri-image-line ms-2 me-3 fs-5"></i>লিপিমীম</a
-      >
-      <small class="mt-3 ms-4 text-muted">অন্যান্য</small>
-      <a href="" class="list-group-item list-group-item-action ps-4 border-0"
-        ><i class="ri-history-line ms-2 me-3 fs-5"></i>আজকের দিনে</a
+        ><i class="ri-question-answer-line ms-2 me-3 fs-5"></i>Forum</a
       >
       <a href="" class="list-group-item list-group-item-action ps-4 border-0"
-        ><i class="ri-live-line ms-2 me-3 fs-5"></i>লিপিকরণ লাইভ</a
+        ><i class="ri-image-line ms-2 me-3 fs-5"></i>Meme</a
+      >
+      <small class="mt-3 ms-4 text-muted">Others</small>
+      <a href="" class="list-group-item list-group-item-action ps-4 border-0"
+        ><i class="ri-history-line ms-2 me-3 fs-5"></i>Today's</a
       >
       <a href="" class="list-group-item list-group-item-action ps-4 border-0"
-        ><i class="ri-briefcase-4-line ms-2 me-3 fs-5"></i>ক্যারিয়ার্স</a
+        ><i class="ri-live-line ms-2 me-3 fs-5"></i>Lipikoron Live</a
       >
       <a href="" class="list-group-item list-group-item-action ps-4 border-0"
-        ><i class="ri-calendar-event-line ms-2 me-3 fs-5"></i>ইভেন্টস</a
+        ><i class="ri-briefcase-4-line ms-2 me-3 fs-5"></i>Career</a
+      >
+      <a href="" class="list-group-item list-group-item-action ps-4 border-0"
+        ><i class="ri-calendar-event-line ms-2 me-3 fs-5"></i>Events</a
       >
     </ul>
   </section>
