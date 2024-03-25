99d00ecd6c3201019fe86e18ab9184fa355e9b03
Shahadat Hossain <33016130+sdtHossain@users.noreply.github.com>
2024-03-26 01:53:40 +0600

    removed test dropdown button

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -23,22 +23,6 @@
           <i class="ri-user-line"></i>
         </button>
 
-        <div class="dropdown">
-          <button
-            class="btn btn-secondary dropdown-toggle"
-            type="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Dropdown button
-          </button>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </div>
-
         <div class="collapse navbar-collapse">
           <form class="ms-5 me-auto input-group" style="width: 32%">
             <input
