d31d3f3feff61d31b3013e04c8d8be83236144a8
Shahadat Hossain <33016130+sdtHossain@users.noreply.github.com>
2024-03-26 01:26:38 +0600

    added basic layouts of lipikoron

---
+++ components/AppCategory.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <ul
+      class="bg-white border-bottom border-2 border-primary shadow-sm d-flex overflow-auto m-0 p-3 link-list"
+    >
+      <button
+        class="btn btn-sm bg-primary text-white rounded-pill shadow-sm px-3 me-2"
+      >
+        <i class="ri-calendar-event-line me-1"></i>অন্যান্য
+      </button>
+      <a href="" class="text-decoration-none text-dark"
+        ><li class="d-inline-block rounded-pill px-3 py-1">সাহিত্য</li></a
+      >
+      <a href="" class="text-decoration-none text-dark"
+        ><li class="d-inline-block rounded-pill px-3 py-1">ইতিহাস</li></a
+      >
+      <a href="" class="text-decoration-none text-dark"
+        ><li class="d-inline-block rounded-pill px-3 py-1">দার্শনিক</li></a
+      >
+      <a href="" class="text-decoration-none text-dark"
+        ><li class="d-inline-block rounded-pill px-3 py-1">গল্প</li></a
+      >
+      <a href="" class="text-decoration-none text-dark"
+        ><li class="d-inline-block rounded-pill px-3 py-1">বিজ্ঞান</li></a
+      >
+      <a href="" class="text-decoration-none text-dark"
+        ><li class="d-inline-block rounded-pill px-3 py-1">প্রযুক্তি</li></a
+      >
+      <a href="" class="text-decoration-none text-dark"
+        ><li class="d-inline-block rounded-pill px-3 py-1">টিউটোরিয়াল</li></a
+      >
+      <a href="" class="text-decoration-none text-dark"
+        ><li class="d-inline-block rounded-pill px-3 py-1">রিভিউ</li></a
+      >
+      <a href="" class="text-decoration-none text-dark"
+        ><li class="d-inline-block rounded-pill px-3 py-1">নিউজ</li></a
+      >
+      <a href="" class="text-decoration-none text-dark"
+        ><li class="d-inline-block rounded-pill px-3 py-1">অন্যান্য</li></a
+      >
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ components/AppSidebar.vue
@@ -0,0 +1,62 @@
+<template>
+  <section
+    class="bg-white d-none d-lg-block position-fixed shadow-sm p-0 sidenav"
+  >
+    <ul class="list-group">
+      <a
+        href=""
+        class="list-group-item list-group-item-action ps-4 border-0 active-tab"
+        ><i class="ri-home-4-line ms-2 me-3 fs-5"></i>লিপিফিড</a
+      >
+      <a href="" class="list-group-item list-group-item-action ps-4 border-0"
+        ><i class="ri-dashboard-line ms-2 me-3 fs-5"></i>ড্যাশবোর্ড</a
+      >
+      <a href="" class="list-group-item list-group-item-action ps-4 border-0"
+        ><i class="ri-quill-pen-line ms-2 me-3 fs-5"></i>নতুন লিপি</a
+      >
+      <small class="mt-3 ms-4 text-muted">অ্যাক্টিভিটি</small>
+      <a href="" class="list-group-item list-group-item-action ps-4 border-0"
+        ><i class="ri-chat-1-line ms-2 me-3 fs-5"></i>লিপিচ্যাট</a
+      >
+      <a href="" class="list-group-item list-group-item-action ps-4 border-0"
+        ><i class="ri-trophy-line ms-2 me-3 fs-5"></i>লিডারবোর্ড</a
+      >
+      <a href="" class="list-group-item list-group-item-action ps-4 border-0"
+        ><i class="ri-book-mark-line ms-2 me-3 fs-5"></i>বুকমার্কস</a
+      >
+      <a href="" class="list-group-item list-group-item-action ps-4 border-0"
+        ><i class="ri-gift-line ms-2 me-3 fs-5"></i>রিওয়ার্ড</a
+      >
+      <small class="mt-3 ms-4 text-muted">এক্সপ্লোর</small>
+      <a href="" class="list-group-item list-group-item-action ps-4 border-0"
+        ><i class="ri-pencil-ruler-line ms-2 me-3 fs-5"></i>লিপিশিখন</a
+      >
+      <a href="" class="list-group-item list-group-item-action ps-4 border-0"
+        ><i class="ri-shopping-bag-line ms-2 me-3 fs-5"></i>লিপিশপ</a
+      >
+      <a href="" class="list-group-item list-group-item-action ps-4 border-0"
+        ><i class="ri-question-answer-line ms-2 me-3 fs-5"></i>লিপিফোরাম</a
+      >
+      <a href="" class="list-group-item list-group-item-action ps-4 border-0"
+        ><i class="ri-image-line ms-2 me-3 fs-5"></i>লিপিমীম</a
+      >
+      <small class="mt-3 ms-4 text-muted">অন্যান্য</small>
+      <a href="" class="list-group-item list-group-item-action ps-4 border-0"
+        ><i class="ri-history-line ms-2 me-3 fs-5"></i>আজকের দিনে</a
+      >
+      <a href="" class="list-group-item list-group-item-action ps-4 border-0"
+        ><i class="ri-live-line ms-2 me-3 fs-5"></i>লিপিকরণ লাইভ</a
+      >
+      <a href="" class="list-group-item list-group-item-action ps-4 border-0"
+        ><i class="ri-briefcase-4-line ms-2 me-3 fs-5"></i>ক্যারিয়ার্স</a
+      >
+      <a href="" class="list-group-item list-group-item-action ps-4 border-0"
+        ><i class="ri-calendar-event-line ms-2 me-3 fs-5"></i>ইভেন্টস</a
+      >
+    </ul>
+  </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
