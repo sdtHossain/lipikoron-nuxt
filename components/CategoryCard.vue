f74c84e8c7df79e2ce2e9ba935fcde9606d90348
Shahadat Hossain <33016130+sdtHossain@users.noreply.github.com>
2024-03-26 02:19:10 +0600

    created category card

---
+++ components/CategoryCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <!-- Recent Items -->
+    <h4 class="mt-3"><i class="ri-history-line me-1"></i>{{ categoryName }}</h4>
+    <section class="grid template250">
+      <ProductCard />
+
+      <ProductCard />
+    </section>
+
+    <!-- see more -->
+    <div class="text-center my-3">
+      <button
+        class="btn bg-white border-primary rounded-pill fs-7 px-3 btn-outline-primary-light"
+      >
+        See More <i class="ri-arrow-down-s-line"></i>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  categoryName: {
+    type: String,
+    default: "Recent",
+  },
+});
+</script>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,7 @@
 <template>
-  <div>
-    <!-- Recent Items -->
-    <h4 class="mt-3"><i class="ri-history-line me-1"></i>Recent</h4>
-    <section class="grid template250">
-      <ProductCard />
-
-      <ProductCard />
-    </section>
-
-    <!-- see more -->
-    <div class="text-center my-3">
-      <button
-        class="btn bg-white border-primary rounded-pill fs-7 px-3 btn-outline-primary-light"
-      >
-        See More <i class="ri-arrow-down-s-line"></i>
-      </button>
-    </div>
-  </div>
+  <CategoryCard category-name="Featured" />
+  <CategoryCard category-name="Recent" />
+  <CategoryCard category-name="Regular" />
 </template>
 
 <script setup lang="ts"></script>
