e1bd27e8920fc4006d380dffaaf36d713a1ae164
Shahadat Hossain <33016130+sdtHossain@users.noreply.github.com>
2024-03-27 00:09:22 +0600

    added category card api

---
+++ app.config.ts
@@ -0,0 +1,5 @@
+export default defineAppConfig({
+  theme: {
+    base_url: 'http://127.0.0.1:8000/api'
+  }
+})
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,23 @@
 <template>
-  <CategoryCard category-name="Featured" />
-  <CategoryCard category-name="Recent" />
-  <CategoryCard category-name="Regular" />
+  <CategoryCard
+    v-for="(category, index) in categories.categories"
+    :key="index"
+    :category-name="category.title"
+  />
+  <!-- <CategoryCard category-name="Recent" />
+  <CategoryCard category-name="Regular" /> -->
   <SeriesItems />
   <BestAuthor />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const appConfig = useAppConfig();
+console.log(appConfig.theme.base_url);
+
+const { data: categories } = await $fetch(
+  `${appConfig.theme.base_url}/categories`
+);
+console.log(categories);
+</script>
 
 <style scoped></style>
